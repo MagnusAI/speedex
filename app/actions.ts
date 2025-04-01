6476faebbd56aef1ad9d2e3c51721f90a5bf699f
arnildtech <arnild.private@gmail.com>
2025-04-01 20:49:55 +0200

    vite init

--- app/actions.ts
+++
@@ -1,9 +0,0 @@
-import { dogData } from "@/data/dogs";
-
-export function getDogs() {
-  return dogData;
-}
-
-export function getDog(name: string) {
-  return dogData.find((dog) => dog.name.includes(name));
-}
