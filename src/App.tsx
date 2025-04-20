9795e62e0d12960e03121c21a5e0eab91a8fccb3
arnildtech <arnild.private@gmail.com>
2025-04-20 23:29:04 +0200

    build attempt 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
-import { theme } from '@/styles/theme'
-import Home from '@/pages/home'
-import DogsAdd from '@/pages/dogs-add'
-import Login from '@/pages/login'
-import ProtectedRoute from '@/components/protected-route'
-import Dogs from '@/pages/dogs'
-import DogDetails from '@/pages/dog-details'
+import { theme } from './styles/theme'
+import Home from './pages/home'
+import DogsAdd from './pages/dogs-add'
+import Login from './pages/login'
+import ProtectedRoute from './components/protected-route'
+import Dogs from './pages/dogs'
+import DogDetails from './pages/dog-details'
 
 function App() {
   return (
